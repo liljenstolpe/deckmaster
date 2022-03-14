96074fbc1d07a2a63a54176add8191f8f894efa0
Christopher LILJENSTOLPE <cdl@asgaard.org>
2022-03-14 15:17:19 -0700

    chaged to notosans font

--- fonts.go
+++ fonts.go
@@ -86,19 +86,19 @@ func loadFont(name string) (*truetype.Font, error) {
 
 func init() {
 	var err error
-	ttfFont, err = loadFont("Roboto-Regular.ttf")
+	ttfFont, err = loadFont("NotoSans-Regular.ttf")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error loading font:", err)
 		os.Exit(1)
 	}
 
-	ttfThinFont, err = loadFont("Roboto-Thin.ttf")
+	ttfThinFont, err = loadFont("NotoSans-Thin.ttf")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error loading font:", err)
 		os.Exit(1)
 	}
 
-	ttfBoldFont, err = loadFont("Roboto-Bold.ttf")
+	ttfBoldFont, err = loadFont("NotoSans-Bold.ttf")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error loading font:", err)
 		os.Exit(1)
